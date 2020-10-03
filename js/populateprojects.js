720092ad7ca9fa840ff4e8e96601ee1daa1859a7
Jasmine Webb <jmwebb.94@gmail.com>
2020-10-02 23:32:47 -0700

    Grey out links that lead nowhere

--- js/populateprojects.js
+++ js/populateprojects.js
@@ -15,27 +15,39 @@ export default class PopulateProjects {
 
   generateProjects() {
     this.projects.forEach(project => {
+      // Create new project card
       const newCard = document.createElement("div");
       newCard.setAttribute("class", "card");
 
+      // Set image and name
       newCard.innerHTML = `
         <img src="${project.thumbnail}" alt="project thumbnail">
         <h3>${project.name}</h3>
       `;
 
+      // Set description with proper formatting
       project.description.forEach(paragraph => {
         newCard.innerHTML += `<p>${paragraph}</p>`
       });
 
-      newCard.innerHTML += `
-        <div>
-          [
-          <a href="${project.demo}" target="_blank">Demo</a>,
-          <a href="${project.source}" target="_blank">Source</a>
-          ]
-        </div>
-      `;
+      // Set links
+      let links = "";
 
+      if (project.demo) {
+        links = `<a href="${project.demo}" target="_blank">Demo</a>, `;
+      } else {
+        links = `<span class="no-link">demo</span>, `;
+      }
+
+      if (project.source) {
+        links += `<a href="${project.source}" target="_blank">Source</a>`
+      } else {
+        links += `<span class="no-link">source</span>`;
+      }
+      
+      newCard.innerHTML += `<div>[ ${links} ]</div>`;
+
+      // Add to DOM
       this.element.appendChild(newCard);
     });
   }
