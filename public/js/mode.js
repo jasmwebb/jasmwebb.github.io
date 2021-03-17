409f77ea1cbbd1aad8a60dd30409151e68483744
Jasmine Webb <jmwebb.94@gmail.com>
2021-03-17 13:08:07 -0700

    Remove public directory from .gitignore

---
+++ public/js/mode.js
@@ -0,0 +1,22 @@
+const userPrefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const body = document.body;
+const modeButton = document.getElementById("mode-button");
+const mode = localStorage.getItem("mode");
+
+if (!mode) {
+  // User's first visit, no mode saved, go by system settings
+  const userPreference = userPrefersDark ? "dark" : "light";
+  body.classList.add(userPreference);
+  localStorage.setItem("mode", body.classList.value);
+} else if (mode === "dark") {
+  body.classList.add("dark");
+} else if (mode === "light") {
+  body.classList.add("light");
+}
+
+modeButton.onclick = () => {
+  body.classList.toggle("light");
+  body.classList.toggle("dark");
+
+  localStorage.setItem("mode", body.classList.value);
+};
